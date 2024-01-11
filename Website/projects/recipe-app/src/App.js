b0f8b0c3d9e57add987337989738f63b39003c80
Elias <elias.ferket@vdabcampus.be>
2024-01-11 21:46:53 +0100

    test Recipe app

--- Website/projects/recipe-app/src/App.js
+++ Website/projects/recipe-app/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import AddRecipe from './AddRecipe';
+import RecipeList from './RecipeList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Recipe App</h1>
+      {/* Other components or content */}
+      <AddRecipe />
+      <RecipeList />
     </div>
   );
 }
---
+++ Website/projects/recipe-app/src/config.js
@@ -0,0 +1,4 @@
+// config.js
+const API_BASE_URL = 'http://localhost:5000/api'; // Update the URL to match your server setup
+
+export default API_BASE_URL;
