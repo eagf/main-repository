538ff26d0fb675ff03052cf41ff14cb923ff5266
Elias <elias.ferket@vdabcampus.be>
2024-01-16 09:26:46 +0100

    test

--- Website/projects/recipe-app/server.js
+++ Website/projects/recipe-app/server.js
@@ -7,10 +7,9 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
 const app = express();
+
 app.use(cors());
 app.use(bodyParser.json());
-
-
 app.use(cookieParser());
 
 async function initializeServer() {
