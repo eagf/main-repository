ca51c846ee96d9f04e3406a3fea1373adc81b93c
Elias <elias.ferket@vdabcampus.be>
2024-01-25 18:34:29 +0100

    css changes logo

---
+++ Website/projects/logopedie/scripts.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// window.addEventListener('scroll', function () {
+//     var headerList = document.getElementById('header-list');
+//     var scrolled = window.scrollY;
+
+//     if (scrolled > 0) {
+//         headerList.style.display = 'none';
+//     } else {
+//         headerList.style.display = 'flex';
+//     }
+// });
