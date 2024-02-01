d837fb04515aaab8ae2caa505bf122cc082a7b8a
Elias <elias.ferket@vdabcampus.be>
2024-02-01 19:33:16 +0100

    logopedie css

--- Website/projects/logopedie/index.js
+++ Website/projects/logopedie/index.js
@@ -19,8 +19,43 @@ toggleIndicator();
 
 indicator.addEventListener('click', function() {
     window.scrollBy({
-        top: 700, 
+        top: 900, 
         left: 0,
         behavior: 'smooth' 
     });
+});
+
+// hamburger icon menu toggle
+
+function toggleNav() {
+    const nav = document.getElementById('header-list');
+    const hamburger = document.getElementById('hamburger-icon');
+    const image = document.querySelector('.header-img-container');
+
+    nav.style.display = nav.style.display === 'flex' ? 'none' : 'flex';
+    nav.style.marginTop = nav.style.display === 'flex' ? '10px' : '0';
+    nav.style.marginBottom = nav.style.display === 'flex' ? '20px' : '0';
+    image.style.display = nav.style.display === 'flex' ? 'flex' : 'none';
+
+    hamburger.classList.toggle('active');
+}
+
+function checkWidthScreen() {
+    var breedteScherm = window.innerWidth;
+
+    var header = document.getElementById('header-list');
+    const image = document.querySelector('.header-img-container');
+
+    if (breedteScherm > 768) {
+        header.style.display = 'flex';
+        image.style.display = 'flex';
+    }
+    else {
+        header.style.display = 'none';
+        image.style.display = 'none';
+    }
+}
+
+window.addEventListener('resize', function () {
+    checkWidthScreen();
 });
