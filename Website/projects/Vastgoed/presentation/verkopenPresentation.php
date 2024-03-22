063c6a2073dc644137e3e8ffe74a3c5dd8ec2192
eliasferket <eliasferket@gmail.com>
2024-03-22 16:40:33 +0100

    css vastgoed

--- Website/projects/Vastgoed/presentation/verkopenPresentation.php
+++ Website/projects/Vastgoed/presentation/verkopenPresentation.php
@@ -12,14 +12,107 @@
 
 <body>
     <div id="wrapper">
-
         <?php include("includes/header.php"); ?>
 
+        <div id="main-wrapper">
+            <section class="section-intro">
+                <h1>Verkopen</h1>
+                <p>
+                    Uw eigen pand, een pand van familie of een investering verkopen …
+                    het blijft een belangrijk moment.
+                    Daarom hebt u nood aan professionele begeleiding voor een zorgeloze verkoop.
+                    Dit biedt Libeer Vastgoed u door vanaf de start te zorgen voor een
+                    volledige ontlasting:
+                </p>
+                <ul>
+                    <li>een persoonlijke begeleiding</li>
+                    <li>24/7 bereikbaar zijn voor al uw vragen en noden</li>
+                    <li>een jarenlange ervaring op de vastgoedmarkt</li>
+                    <li>de beste prijs voor uw pand weten te verzilveren</li>
+                </ul>
+            </section>
 
+            <section class="section-content">
+                <div class="content-image">
+                    <img src="assets/img/logo.png" alt="">
+                </div>
+                <div class="content-text">
+                    <h2>
+                        Hoe zorgt Libeer Vastgoed voor een zorgeloze en succesvolle verkoop
+                        van uw pand?
+                    </h2>
+                    <p>
+                        In een verkennend gesprek, waarvoor wij persoonlijk bij u langskomen,
+                        brengen we samen eerst uw behoeften in kaart.
+                        Daarna gaan we aan de hand van de juiste vastgoedtools onderbouwd
+                        met onze brede kennis ervaring over tot het bepalen van een marktconforme
+                        prijs van uw pand.
+                    </p>
+                    <p class="italic-text">
+                        Wenst u een vrijblijvende waardebepaling klik 
+                        <a href="contact.php">hier</a>
+                         voor het contactformulier.
+                    </p>
+                    <p>
+                        Vooraleer over te gaan tot het bepalen van de verkoopstrategie,
+                        vragen wij eerst en vooral alle nodige verkoopdocumenten aan want
+                        pas dan kunnen wij u die zorgeloze verkoop verzekeren.
+                    </p>
+                </div>
+            </section>
 
+            <section class="section-content">
+                <div class="content-text">
+                    <p>
+                        Aan de hand van alle verkregen informatie wordt de verkoopstrategie
+                        van uw pand bepaald:
+                    </p>
+                    <ul>
+                        <li>het bepalen van het ‘koop’publiek/doelpubliek</li>
+                        <li>de visuele en inhoudelijke presentatie van uw pand</li>
+                        <li>het aanbieden van uw pand via onze website, vastgoedwebsites,
+                            sociale media, netwerk, … </li>
+                    </ul>
+                    <p>
+                        Zo geven wij u een groot bereik van potentiële kopers.
+                    </p>
+                </div>
+                <div class="content-image">
+                    <img src="assets/img/logo.png" alt="">
+                </div>
+            </section>
+
+            <section class="section-content">
+                <div class="content-image">
+                    <img src="assets/img/logo.png" alt="">
+                </div>
+                <div class="content-text">
+                    <p>
+                        De succesvolle verkoop uw pand bereiken zodra de vooropgestelde prijs behaald is.
+                        Op dat moment gaan wij direct over tot het opstellen van de
+                        verkoopovereenkomst in samenspraak met de notaris van beide partijen.
+                    </p>
+                    <p>
+                        Ook bij de laatste stap in uw verkoopproces
+                        ‘het tekenen van de akte bij de notaris’
+                        zorgen wij voor een vlekkeloze eigendomsoverdracht
+                        (overdracht van documenten, energie, sleutels, …).
+                    </p>
+                </div>
+            </section>
+
+            <section class="section-conclusion">
+                <p>
+                    Kortom Libeer Vastgoed staat voor de zorgeloze verkoop van uw
+                    pand met het beste resultaat!
+                </p>
+            </section>
+        </div>
     </div>
 
     <?php include('includes/footer.php'); ?>
 </body>
+
+
 
 </html>
