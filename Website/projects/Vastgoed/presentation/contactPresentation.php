3267cf887143de4a51c0eb77dc3cc2bcdfd5b8d5
eliasferket <eliasferket@gmail.com>
2024-03-29 16:47:21 +0100

    css vastgoed

--- Website/projects/Vastgoed/presentation/contactPresentation.php
+++ Website/projects/Vastgoed/presentation/contactPresentation.php
@@ -12,10 +12,36 @@
 
 <body>
     <div id="wrapper">
-
         <?php include("includes/header.php"); ?>
 
-
+        <div class="contact-container">
+            <form class="contact-form" action="submit_contact_form.php" method="post">
+                <div class="form-field">
+                    <label for="naam">Naam</label>
+                    <input type="text" id="naam" name="naam" required>
+                </div>
+                <div class="form-field">
+                    <label for="voornaam">Voornaam</label>
+                    <input type="text" id="voornaam" name="voornaam" required>
+                </div>
+                <div class="form-field">
+                    <label for="email">E-mailadres</label>
+                    <input type="email" id="email" name="email" required>
+                </div>
+                <div class="form-field">
+                    <label for="onderwerp">Onderwerp</label>
+                    <input type="text" id="onderwerp" name="onderwerp" required>
+                </div>
+                <div class="form-field">
+                    <label for="message">Bericht</label>
+                    <textarea id="message" name="message" rows="4" required></textarea>
+                </div>
+                <div class="form-submit">
+                    <p class="submit-text">Verstuur</p>
+                    <img src="./assets/img/Bereklauw.png" alt="Verstuur" class="submit-button">
+                </div>
+            </form>
+        </div>
 
     </div>
 
