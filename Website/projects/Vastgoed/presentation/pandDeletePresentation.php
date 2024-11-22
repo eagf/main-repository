310798749191c958a4b25e2f5eee8e796f2e0f18
eliasferket <eliasferket@gmail.com>
2024-11-22 15:03:50 +0100

    pand delete remove

--- Website/projects/Vastgoed/presentation/pandDeletePresentation.php
+++ Website/projects/Vastgoed/presentation/pandDeletePresentation.php
@@ -24,9 +24,6 @@
                 echo "<p class='success'>" . $message . "</p>";
             } ?>
             <h2>Selecteer het pand om te verwijderen</h2>
-            <!-- CONTROL STILL IN CODE!!!!!!!!!!!!!!!! -->
-            <h3 style="color: red">De eerste 5 voorbeelden zijn nog niet te verwijderen</h3>
-            <!-- CONTROL STILL IN CODE!!!!!!!!!!!!!!!! -->
             <form action="pandDelete.php" method="POST">
                 <label for="pandID">Selecteer een pand:</label>
                 <select id="pandID" name="pandID">
