82b6ca8123eec99d98f8512562e6fb824ca6a53a
eliasferket <eliasferket@gmail.com>
2025-04-18 17:07:32 +0200

    correction images FIX LATER PERMANENT

--- Website/projects/Vastgoed/images.php
+++ Website/projects/Vastgoed/images.php
@@ -8,6 +8,10 @@ include('data/sessionValidationScript.php');
 require_once('data/DBConfig.php');
 require_once('data/functions.php');
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $message = "";
 
 if (isset($_GET["message"])) {
